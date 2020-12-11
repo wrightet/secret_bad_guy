5b3dbd8be1c117059cc309c22893952e6900c3d3
Ethan Wright <wrightet707@gmail.com>
2020-12-10 16:40:42 -0800

    attempt to get front end to work

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/Root'
+document.addEventListener('DOMcontentLoaded', ()=>{
+    ReactDOM.render(<div>React works</div>, document.getElementById('root'))
+})
---
+++ frontend/store/store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk';
+// import logger from 'redux-logger'
+
+import rootReducer from '../src/reducers/root_reducer'
+
+const configureStore = (preloadedState = {}) => {
+    const middlewareEnhancer = applyMiddleware(thunk);
+    const composedEnhancer = composeWithDevTools(middlewareEnhancer);
+
+    return (
+        createStore(
+            rootReducer,
+            preloadedState,
+            composedEnhancer
+        )
+    )
+};
+
+export default configureStore;
